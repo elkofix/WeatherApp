567c5de5db19c16bd2b5178fc7b5c6f3de056243
elkofix <holasoygermamwow@gmail.com>
2024-06-17 17:23:50 -0500

    WIP:#1: Adding first api call and connecting it to index

---
+++ js/api.js
@@ -0,0 +1,15 @@
+const apiKey = "3550f4bba913220283bbcf1fdb12fe72";
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=cali&appid=${apiKey}}`;
+
+
+
+
+
+
+
+async function checkWeather(){
+    const response = await fetch(apiUrl);
+    var data = await response.json()
+
+    console.log(data.coord)
+}
